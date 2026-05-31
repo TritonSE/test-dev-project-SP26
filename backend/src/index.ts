7e31982af2b06a77b573d6d769d4b6f70cb64bc5
imontanez19 <imontanez@ucsd.edu>
2026-05-30 19:40:39 -0700

    Check Point for Code Review

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,8 +1,10 @@
 import express from "express";
 
 import type { Request, Response } from "express";
+import membersRoute from "./routes/members";
 
 const app = express();
+app.use("/api/members", membersRoute);
 const PORT = process.env.PORT ?? 3001;
 
 app.use(express.json());
